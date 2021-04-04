bd8d3fa643dfeaa8227c38c47c8e2bfb5f4a44a1
Faraon78 <olgaapt78@gmail.com>
2021-04-04 21:48:36 +0300

    04_04

---
+++ IShop/IShop.js
@@ -0,0 +1,33 @@
+﻿var ProductTable = React.createClass({
+
+  displayName: 'ProductTable',
+
+  getDefaultProps: function() {
+    return { question: "Вопрос ни о чём" }
+  },
+
+  render: function() {
+    var productsCode=[];
+    
+    for ( var a=0; a<this.props.products.length; a++ ) {
+      var product=this.props.products[a];
+      var productCode=        
+        React.DOM.tbody({key:product.code,className:'Product'},
+          React.DOM.td({className:'NameProd'},product.nameProd),
+          React.DOM.td({className:'Cost'},product.cost),
+          React.DOM.td({className:'Count'},product.count),
+          React.DOM.td({className:'Photo'},
+            //React.DOM.img(null, style.src=product.photo)
+            ),
+          
+        );
+      productsCode.push(productCode);
+    }
+    return React.DOM.div( {className:'ProductTable'}, 
+      React.DOM.h1( {className:'Header'}, 'Список товаров'), 
+      React.DOM.table( {className:'Table'}, productsCode ),
+      
+    );
+  },
+
+});
