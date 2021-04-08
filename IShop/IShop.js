308dbe5e324911ee70244cf1ea18a6bf5e9f82cf
Faraon78 <olgaapt78@gmail.com>
2021-04-08 22:03:46 +0300

    08_04

--- IShop/IShop.js
+++ IShop/IShop.js
@@ -2,31 +2,38 @@
 
   displayName: 'ProductTable',
 
-  getDefaultProps: function() {
-    return { question: "Вопрос ни о чём" }
-  },
-
+  
   render: function() {
+    
+    
+
     var productsCode=[];
     
     for ( var a=0; a<this.props.products.length; a++ ) {
       var product=this.props.products[a];
       var productCode=        
-        React.DOM.tbody({key:product.code,className:'Product'},
+        React.DOM.tr({key:product.code,className:'Product'},
           React.DOM.td({className:'NameProd'},product.nameProd),
           React.DOM.td({className:'Cost'},product.cost),
           React.DOM.td({className:'Count'},product.count),
           React.DOM.td({className:'Photo'},
-            //React.DOM.img(null, style.src=product.photo)
-            ),
-          
+            React.DOM.img({src: product.photo})
+            ),          
         );
       productsCode.push(productCode);
     }
     return React.DOM.div( {className:'ProductTable'}, 
-      React.DOM.h1( {className:'Header'}, 'Список товаров'), 
-      React.DOM.table( {className:'Table'}, productsCode ),
+      React.DOM.h1( {className:'Header'}, this.props.shop), 
+      React.DOM.table({className:'Table'},
+        React.DOM.tr({className:'TableHead'}, 
+          React.DOM.th({className:'NameProd'}, this.props.tableHead.nameProd),
+          React.DOM.th({className:'Cost'}, this.props.tableHead.cost),
+          React.DOM.th({className:'Count'}, this.props.tableHead.count),
+          React.DOM.th({className:'Photo'}, this.props.tableHead.photo),
+        ), 
+        React.DOM.tbody({className:'TableRow'}, productsCode ),
       
+      )
     );
   },
 
