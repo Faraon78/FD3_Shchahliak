a0910b4fc489e9699d6cc32c7f1669ac2feafa17
Faraon78 <olgaapt78@gmail.com>
2021-04-16 23:25:14 +0300

    16_04

---
+++ Filter/Filter.js
@@ -0,0 +1,36 @@
+﻿var FilterWord = React.createClass({
+
+  displayName: 'FilterWord',
+
+  propTypes: {
+    filterMode: React.PropTypes.string.isRequired,  //start - изначальный список слов, filterString - отфильтрованный список, sort - отсортированный список
+  },  
+  getInitialState: function() {
+    return{
+      filterMode:start,
+      sortLines:false
+    }
+  }
+    
+    filterLines: function(){
+      let lines=this.props.words.slice(); // делаем плоскую копию всех слов, т.к. возможно будем сортировать массив
+      if ( this.state.filterMode=="filterString")
+        lines=lines.filter( line => line.indexOf(this.state.filterString)!=-1 );
+      if ( this.state.filterMode=='sort')
+        lines.sort();
+      this.setState( { lines:lines } );
+    },
+
+    render: function() {
+    return React.DOM.div( 
+      React.DOM.div({className:'FilterString'}, 
+        React.DOM.input( {type:'checkbox'}), 
+        React.DOM.input( {type:'text'}, onChange:this.filterLines),
+        React.DOM.input({type:'button',defaultValue:'сброс'} ),      
+      ),
+      React.DOM.select({className:'FilerWord'},
+        React.DOM.options(null, lines)),
+    )
+  }
+
+});
---
+++ IShop2/IShop2.js
@@ -0,0 +1,16 @@
+﻿var ShopTable = React.createClass({
+
+  displayName: 'ShopTable',
+
+  
+  render: function() {
+        
+    return React.DOM.div( {className:'ShopTable'}, 
+      React.DOM.h1( {className:'Header'}, this.props.shop), 
+      React.createElement(ProductTable, {products:productArr, tableHead:productHead},
+           
+      )
+    );
+  },
+
+});
