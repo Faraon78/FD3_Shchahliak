94f845f347351bb982581ba39e83f84b5cf76a8f
Faraon78 <olgaapt78@gmail.com>
2021-04-20 18:52:31 +0300

    20_04

--- Filter/Filter.js
+++ Filter/Filter.js
@@ -3,34 +3,49 @@
   displayName: 'FilterWord',
 
   propTypes: {
-    filterMode: React.PropTypes.string.isRequired,  //start - изначальный список слов, filterString - отфильтрованный список, sort - отсортированный список
+    words: React.PropTypes.array.isRequired,  
+    sorted: React.PropTypes.bool,
+    filtered: React.PropTypes.string,
+    lines: React.PropTypes.array,
   },  
+
   getInitialState: function() {
     return{
-      filterMode:start,
-      sortLines:false
+      sorted:false,
+      filtered:"",
+      lines:[],
     }
-  }
-    
-    filterLines: function(){
-      let lines=this.props.words.slice(); // делаем плоскую копию всех слов, т.к. возможно будем сортировать массив
-      if ( this.state.filterMode=="filterString")
-        lines=lines.filter( line => line.indexOf(this.state.filterString)!=-1 );
-      if ( this.state.filterMode=='sort')
-        lines.sort();
-      this.setState( { lines:lines } );
-    },
+  },
+
+  checkboxclicked: function(EO){
+    this.setState({sorted:EO.target.checked}, processlines)
+  },
+   
+  edittext : function(EO){
+    this.setState( { filtered:EO.target.value} );
+  },
+
+  processlines: function(){
+    let res=this.props.words.slice(); 
+      if ( this.state.filtered){
+         res=res.filter( line => line.indexOf(this.state.filtered)!=-1 );
+      }
+      if ( this.state.sorted=="true"){
+        res.sort();
+      }
+    this.setState( { lines:this.res} ) 
+  },
 
     render: function() {
     return React.DOM.div( 
       React.DOM.div({className:'FilterString'}, 
-        React.DOM.input( {type:'checkbox'}), 
-        React.DOM.input( {type:'text'}, onChange:this.filterLines),
+        React.DOM.input( {type:'checkbox', onclick:this.checkboxclicked}), 
+        React.DOM.input( {type:'text', onchange:this.edittext}),
         React.DOM.input({type:'button',defaultValue:'сброс'} ),      
       ),
-      React.DOM.select({className:'FilerWord'},
-        React.DOM.options(null, lines)),
+      React.DOM.select({className:'FilerWord'}, 
+        React.DOM.option({lines:this.state.lines}),
+      ), 
     )
-  }
-
+    }
 });
