2091da2a26210f7339c190c721c487f3eb5d90be
Faraon78 <olgaapt78@gmail.com>
2021-04-22 21:12:24 +0300

    22_04

--- Filter/Filter.js
+++ Filter/Filter.js
@@ -6,46 +6,64 @@
     words: React.PropTypes.array.isRequired,  
     sorted: React.PropTypes.bool,
     filtered: React.PropTypes.string,
-    lines: React.PropTypes.array,
+    rezult: React.PropTypes.array,
   },  
 
   getInitialState: function() {
     return{
       sorted:false,
       filtered:"",
-      lines:[],
+      rezult:this.props.words,     
     }
+  }, 
+   
+  edittext : function(EO){    
+    this.setState( { filtered:EO.target.value}, this.processlines);
   },
 
-  checkboxclicked: function(EO){
-    this.setState({sorted:EO.target.checked}, processlines)
-  },
-   
-  edittext : function(EO){
-    this.setState( { filtered:EO.target.value} );
+  clear: function(){
+      this.setState({filtered:'', sorted:false}, this.processlines);
+      console.log(this.state.sorted, this.state.filtered )
   },
 
   processlines: function(){
-    let res=this.props.words.slice(); 
+    console.log("запустился Processlines, sorted:"+this.state.sorted, "filtered:"+this.state.filtered);
+    let rez=this.props.words.slice(); 
       if ( this.state.filtered){
-         res=res.filter( line => line.indexOf(this.state.filtered)!=-1 );
+         rez=rez.filter( line => line.indexOf(this.state.filtered)!=-1 );
       }
-      if ( this.state.sorted=="true"){
-        res.sort();
-      }
-    this.setState( { lines:this.res} ) 
+      if ( this.state.sorted){
+        rez=rez.sort()
+      };
+      console.log("rez:"+ rez);  
+    this.setState( { rezult:rez } ) 
+  },
+  checkboxclicked: function(EO){
+    
+    this.setState({sorted:EO.target.checked}, this.processlines)
+    
   },
 
     render: function() {
-    return React.DOM.div( 
-      React.DOM.div({className:'FilterString'}, 
-        React.DOM.input( {type:'checkbox', onclick:this.checkboxclicked}), 
-        React.DOM.input( {type:'text', onchange:this.edittext}),
-        React.DOM.input({type:'button',defaultValue:'сброс'} ),      
-      ),
-      React.DOM.select({className:'FilerWord'}, 
-        React.DOM.option({lines:this.state.lines}),
-      ), 
+     
+      var screenrez=[];   
+      
+      for ( var a=0; a<this.state.rezult.length; a++ ) {
+        var screenline=React.DOM.option({key:a}, this.state.rezult[a]);
+        screenrez.push(screenline);
+      }
+      
+
+    return React.DOM.div( null,
+        React.DOM.div({className:'FilterBlock'},
+      
+        React.DOM.input( {type:'checkbox', onClick:this.checkboxclicked, checked:this.state.sorted}),  
+        React.DOM.input( {type:'text', className:'FilterString', onChange:this.edittext, value:this.state.filtered}),
+        React.DOM.input({type:'button',className:'Button', value:'сброс', onClick:this.clear} ),      
+        ),
+        React.DOM.div( null, 
+        React.DOM.select({className:'FilterWord',size:7}, screenrez),
+        ),  
     )
     }
 });
