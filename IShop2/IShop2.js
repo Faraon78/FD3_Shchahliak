f3ca4c21459365637bc15d0803b911c9b1ee4ed8
Faraon78 <olgaapt78@gmail.com>
2021-04-23 22:26:48 +0300

    23_04_1

--- IShop2/IShop2.js
+++ IShop2/IShop2.js
@@ -4,13 +4,32 @@
 
   
   render: function() {
-        
+    var productsCode=[];
+    
+    for ( var a=0; a<this.props.products.length; a++ ) {
+      var product=this.props.products[a];
+      var productCode=
+        React.createElement(ProductRow, {item:product, key:product.code});       
+        console.log(productCode);  
+      productsCode.push(productCode);
+    }
+      console.log(productsCode);
+
     return React.DOM.div( {className:'ShopTable'}, 
       React.DOM.h1( {className:'Header'}, this.props.shop), 
-      React.createElement(ProductTable, {products:productArr, tableHead:productHead},
-           
-      )
-    );
-  },
+      React.DOM.table({className:'Table'},
+        React.DOM.tbody({className:'TableHead'}, 
+          React.DOM.tr(null,
+            React.DOM.th({className:'NameProd'}, this.props.tableHead.nameProd),
+            React.DOM.th({className:'Cost'}, this.props.tableHead.cost),
+            React.DOM.th({className:'Count'}, this.props.tableHead.count),
+            React.DOM.th({className:'Photo'}, this.props.tableHead.photo),
+            React.DOM.th({className:'Control'}, this.props.tableHead.control),
+          ) 
+        ),        
+        React.DOM.tbody({className:'TableRow'}, productsCode ),
+      ) 
+    )
+  }
 
 });
--- IShop2/Product.js
+++ IShop2/Product.js
@@ -1,54 +1,42 @@
-﻿var ProductTable = React.createClass({
+﻿var ProductRow = React.createClass({
 
-  displayName: 'ProductTable',
-
-  marker: function(EO){
-    this.props.product({backgroundcolor:orange});   //// В этой строке выдает ошибку
-    this.setState({products:productsCode});
+  displayName: 'ProductRow',
+  propTypes: {
+    nameProd: React.PropTypes.string,  
+    cost: React.PropTypes.number,
+    count: React.PropTypes.number,
+    rezult: React.PropTypes.array,
   },
 
-  deleteProduct: function (EO){
-    console.log (EO.target.name);
-    let codeDeleteProduct=EO.target.name;
-    let custom=confirm('Вы действительно хотите удалить этот товар из списка?');
-    if (custom==true){
-      productsCode.splice(this.codeDeleteProduct,1);   // В этой строке выдает ошибку
-    }
-    this.setState({products:productsCode});
-  },
+  //marker: function(EO){
+  //  this.props.product({backgroundcolor:orange});   //// В этой строке выдает ошибку
+  //  this.setState({products:productsCode});
+  //},
+
+  //deleteProduct: function (EO){
+  //  console.log (EO.target.name);
+  //  let codeDeleteProduct=EO.target.name;
+  //  let custom=confirm('Вы действительно хотите удалить этот товар из списка?');
+  //  if (custom==true){
+  //    productsCode.splice(this.codeDeleteProduct,1);   // В этой строке выдает ошибку
+  //  }
+  //  this.setState({products:productsCode});
+  //},
   
   render: function() {
-    var productsCode=[];
-    
-    for ( var a=0; a<this.props.products.length; a++ ) {
-      var product=this.props.products[a];
-      var productCode=        
-        React.DOM.tr({key:product.code,className:'Product', onClick:this.marker},
-          React.DOM.td({className:'NameProd'},product.nameProd),
-          React.DOM.td({className:'Cost'},product.cost),
-          React.DOM.td({className:'Count'},product.count),
-          React.DOM.td({className:'Photo'},
-            React.DOM.img({src: product.photo})
-            ),
-          React.DOM.td({className:'Control'},
-          React.DOM.input({type: 'button', value: 'Удалить', name:product.code, onClick:this.deleteProduct})
-            ),  
-          );
-      productsCode.push(productCode);
-    
-  }
-    return React.DOM.table({className:'Table'},
-        React.DOM.tbody({className:'TableHead'}, 
-          React.DOM.tr(null,
-            React.DOM.th({className:'NameProd'}, this.props.tableHead.nameProd),
-            React.DOM.th({className:'Cost'}, this.props.tableHead.cost),
-            React.DOM.th({className:'Count'}, this.props.tableHead.count),
-            React.DOM.th({className:'Photo'}, this.props.tableHead.photo),
-            React.DOM.th({className:'Control'}, this.props.tableHead.control),
-          ) 
-        ),
-        React.DOM.tbody({className:'TableRow'}, productsCode ),
-      
-      )  
-    }         
+      return 
+          React.DOM.tr({key:item.code,className:'Product'},// , onClick:this.marker
+              React.DOM.td({className:'NameProd'},item.nameProd),
+              React.DOM.td(({className:'Cost'},item.cost),
+              React.DOM.td({className:'Count'},item.count),
+              React.DOM.td({className:'Photo'},
+                React.DOM.img({src: item.photo})
+                ),
+              React.DOM.td({className:'Control'},
+                React.DOM.input({type: 'button', value: 'Удалить', name:item.code}) //, onClick:this.deleteProduct
+                ), 
+          )
+          )        
+        }
+             
 });
